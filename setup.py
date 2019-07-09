122a28f1375912e2aab5d2ff8d5162ec2648f403
John <japarker@uchicago.edu>
2019-07-08 21:36:25 -0500

    write config file at installation

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import os
 from setuptools import setup, find_packages
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+import pathlib
 
 NAME = 'git-scan'
 DESCRIPTION = "Scan local or remote git repositories for history divergent from origin"
@@ -18,6 +18,38 @@ REQUIRED = [
     'toml',
 ]
 
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def post_install():
+    config_str = """
+repositories = [
+
+]
+
+ssh = [
+
+]
+    """.strip()
+
+    config_path = pathlib.Path('~/.config/git-scan').expanduser()
+    if not os.path.exists(config_path):
+        with open(config_path, 'w') as f:
+            f.write(config_str)
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        post_install()
+        install.run(self)
+
+class PostDevelopCommand(develop):
+    """Post-installation for installation mode."""
+    def run(self):
+        post_install()
+        develop.run(self)
+
 setup(
     name=NAME,
     version=VERSION,
@@ -31,6 +63,10 @@ setup(
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
     install_requires=REQUIRED,
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
